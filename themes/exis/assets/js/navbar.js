d11504237868ad2e013e4a942ec57696fd1677e0
C L <claycle@claycle.com>
2022-01-26 16:54:26 +0000

    Added example dbconfig and htaccess for later.

--- themes/exis/assets/js/navbar.js
+++ themes/exis/assets/js/navbar.js
@@ -1,8 +1,8 @@
-/* 
-* Navbar Foo 
-* We want to be able to have the navbar "pop" out and stick when scrolled to the top.
-* This requires a little fine control.
-*/
+/*
+ * Navbar Foo
+ * We want to be able to have the navbar "pop" out and stick when scrolled to the top.
+ * This requires a little fine control.
+ */
 
 var startProductBarPos = -1;
 
@@ -16,35 +16,36 @@ function findPosY(obj) {
             obj = obj.offsetParent;
         }
         curtop += obj.offsetTop;
-    }
-    else if (obj.y)
-        curtop += obj.y;
+    } else if (obj.y) curtop += obj.y;
     return curtop;
 }
 
-window.addEventListener('scroll', (event) => {
-    var bar = document.getElementById('thenavbar');
-    if (bar === null) { console.log("thenavbar not assigned"); return; }
+window.addEventListener("scroll", (event) => {
+    var bar = document.getElementById("thenavbar");
+    if (bar === null) {
+        console.log("thenavbar not assigned");
+        return;
+    }
     //var barc = document.getElementById('thenavbarc');
     //console.log("Scroll Navbar", bar);
     if (startProductBarPos < 0) startProductBarPos = findPosY(bar);
 
     if (/*pageYOffset*/ scrollY > startProductBarPos) {
-        bar.classList.add('fixed-top');
-        bar.classList.add('stucky');
-        bar.classList.remove('unstucky');
-       // barc.classList.add('container-fluid');
-       // barc.classList.remove('container');
+        bar.classList.add("fixed-top");
+        bar.classList.add("stucky");
+        bar.classList.remove("unstucky");
+        // barc.classList.add('container-fluid');
+        // barc.classList.remove('container');
     } else {
-        bar.classList.remove('fixed-top');
-        bar.classList.remove('stucky');
-        bar.classList.add('unstucky');
+        bar.classList.remove("fixed-top");
+        bar.classList.remove("stucky");
+        bar.classList.add("unstucky");
         //barc.classList.add('container');
         //barc.classList.remove('container-fluid');
     }
 });
 
-window.addEventListener('load', (event) => {
-    console.log('navbar.js Copyright (c) 2022 claycle.com');
+window.addEventListener("load", (event) => {
+    console.log("navbar.js Copyright (c) 2022 claycle.com");
     var startProductBarPos = -1;
 });
